9ff0857a748fa6cd27bcb0db5cfcf3e7455455b4
Luis Andre de Lima <luislimamn@gmail.com>
2019-06-04 12:58:01 -0300

    Initial Commit

---
+++ GeradorDeNumeros/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GeradorDeNumeros
+//
+//  Created by Luis Lima on 04/06/2019.
+//  Copyright © 2019 Andr3ziim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
