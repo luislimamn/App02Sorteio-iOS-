953bc24f6f0ea8c740200ff67c44db19dc6e6c47
Luis Andre de Lima <luislimamn@gmail.com>
2019-06-05 08:35:16 -0300

    Andr3ziim
    Versão 1.0

--- GeradorDeNumeros/ViewController.swift
+++ GeradorDeNumeros/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numeroGerado: UILabel!
+    
+    @IBAction func gerarNumero(_ sender: Any) {
+        
+        var maximo: Int = 0
+        maximo = 1000
+        
+        var numero: Int = 0
+        numero = Int(arc4random_uniform(UInt32(maximo)))
+        numeroGerado.text = "\(numero)"
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
